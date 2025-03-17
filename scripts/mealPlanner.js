b91f23161a80e6f0dfbbdb0ee901b504b97feca0
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2025-03-16 23:46:02 -0700

    created an api file to store api info used in the meal planner and worked on fetching a meal plan based on user input data

--- scripts/mealPlanner.js
+++ scripts/mealPlanner.js
@@ -1,3 +1,5 @@
+import { apiKey, apiURL } from "./spoonacularAPI.js";
+
 //adding active class to the selected diet button
 const dietButtons = document.querySelectorAll(".diet-btn");
 dietButtons.forEach((button) => {
@@ -7,13 +9,33 @@ dietButtons.forEach((button) => {
   });
 });
 
+let mealPlan = [];
 const generateButton = document.querySelector(".btn-warning");
-generateButton.addEventListener("click", function () {
+generateButton.addEventListener("click", async function () {
   const dietType = document.querySelector(".diet-btn.active").value;
   const calories = document.getElementById("calories").value;
   const mealType = document.querySelector(
     'input[name="planType"]:checked'
   ).value;
 
-  console.log(dietType, calories, mealType);
+  // fetching the meal plan based on user input
+  try {
+    let response;
+    if (mealType === "weekly") {
+      response = await fetch(
+        `${apiURL}apiKey=${apiKey}&timeFrame=week&targetCalories=${calories}&diet=${dietType}`
+      );
+    } else {
+      response = await fetch(
+        `${apiURL}apiKey=${apiKey}&timeFrame=day&targetCalories=${calories}&diet=${dietType}`
+      );
+    }
+
+    const data = await response.json();
+    mealPlan = data;
+    console.log(mealPlan);
+  } catch (error) {
+    console.error("Error:", error);
+  }
 });
+console.log(mealPlan);
