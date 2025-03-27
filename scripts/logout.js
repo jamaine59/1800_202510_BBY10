e1d7deb20313cfb81e540ead2c76b96ef98406eb
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2025-03-26 17:38:17 -0700

    updated firebase imports to follow ES and imported them in the firebaseAPI script and created the logout functionality

---
+++ scripts/logout.js
@@ -0,0 +1,20 @@
+import { auth } from "./firebaseAPI_BBY10.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const logoutButtons = document.querySelectorAll(
+    "#logoutButtonMobile, #logoutButtonDesktop"
+  );
+
+  logoutButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      auth
+        .signOut()
+        .then(() => {
+          window.location.href = "./index.html";
+        })
+        .catch((error) => {
+          console.error("Logout error:", error);
+        });
+    });
+  });
+});
