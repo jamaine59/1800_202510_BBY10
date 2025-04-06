f508b53643ef6a69c28884dd65f6cf510cc28fb9
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2025-04-06 00:35:33 -0700

    added an auth check to protect pages so they cant be used only if a user is logged in

---
+++ scripts/authCheck.js
@@ -0,0 +1,18 @@
+import { auth } from "./firebaseAPI_BBY10.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const protectedPages = [
+    "home.html",
+    "mealPlanner.html",
+    "social.html",
+    "calculator.html",
+  ];
+  const currentPage = window.location.pathname.split("/").pop();
+
+  auth.onAuthStateChanged((user) => {
+    if (!user && protectedPages.includes(currentPage)) {
+      console.warn("Not logged in — redirecting");
+      window.location.href = "index.html";
+    }
+  });
+});
