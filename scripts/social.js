686728c2c195ddeab56377e6e3f000dfc8c9bd55
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2025-04-07 01:38:34 -0700

    refactored meal cards structure using prebuild bootstrap card and applied minor styles

--- scripts/social.js
+++ scripts/social.js
@@ -17,16 +17,14 @@ document.addEventListener("DOMContentLoaded", async () => {
 
       const postElement = document.createElement("div");
       postElement.classList.add("post-card");
-      postElement.innerHTML = `
-        <div class="post-content">
-        <div class="post-header">
-            <span class="post-user">${userName}</span>
-          </div>
-          <h3>${post.name}</h3>
-          <img src="data:image/png;base64,${post.imageUrl}" alt="Meal Image" class="post-image" />
-          <p>${post.description}</p>
+      postElement.innerHTML = `<div class="card card-social-feed" style="width: 18rem;">
+      <h4 style="padding-left:1rem; padding-top: 1rem;">@${userName}</h4>
+        <img src="data:image/png;base64,${post.imageUrl}" class="card-img-top feed-img" alt=${post.name}>
+        <div class="card-body">
+          <h5 class="card-title">${post.name}</h5>
+          <p class="card-text">${post.description}</p>
         </div>
-      `;
+      </div>`;
       postsContainer.appendChild(postElement);
     });
   } catch (err) {
