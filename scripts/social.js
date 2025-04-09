3fe91db6172ae4b2f60c43f62a68e56785f674ad
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2025-04-09 04:05:38 -0700

    changed social feed cards width

--- scripts/social.js
+++ scripts/social.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
       const postElement = document.createElement("div");
       postElement.classList.add("post-card");
-      postElement.innerHTML = `<div class="card card-social-feed" style="width: 18rem;">
+      postElement.innerHTML = `<div class="card card-social-feed" style="width: 16rem;">
       <h4 style="padding-left:1rem; padding-top: 1rem;">@${userName}</h4>
         <img src="data:image/png;base64,${post.imageUrl}" class="card-img-top feed-img" alt=${post.name}>
         <div class="card-body">
